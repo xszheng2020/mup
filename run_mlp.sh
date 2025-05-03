dc4d749d2f9e51d85fc794b8bd4b0c59dc137076
Laz4rz <boronskimikolaj@gmail.com>
2025-05-03 17:07:08 +0200

    adam results and adam plots (rerunning mu adam still, wrong res here)

--- run_mlp.sh
+++ run_mlp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Running MLP experiment..."
-python3 train_mlp.py --model NTKMLP --subset 1
+python3 train_mlp.py --model NTKMLP --subset 1 --optimizer Adam
 
 echo "Running muMLP experiment..."
-python3 train_mlp.py --model muMLP --subset 1
+python3 train_mlp.py --model muMLP --subset 1 --optimizer Adam
 
 echo "All experiments completed."
